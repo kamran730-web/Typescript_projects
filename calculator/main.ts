6e2f3de45a139f6214266b7c2f636e2d13c57dd1
Muhammad_Kamran <kk8085528@gmail.com>
2025-10-06 09:55:11 +0500

    1st project

---
+++ calculator/main.ts
@@ -0,0 +1,31 @@
+import inquirer from "inquirer";
+const ans = await inquirer.prompt([{
+    type:"number",
+    name:"first_number",
+    message:"Enter your First Number : "
+},
+{
+    type:"number",
+    name:"Second_number",
+    message:"Enter your Secoond Number : "
+},
+{
+    type:"list",
+    name:"operators",
+    message:"please select operation type",
+    choices:["addition","subtraction","multiplication","division"]
+}
+])
+if(ans.operators === "addition"){
+    console.log(ans.first_number + ans.Second_number);
+    
+}else if (ans.operators === "subtraction"){
+    console.log(ans.first_number + ans.Second_number);
+    
+}else if (ans.operators === "multiplication"){
+    console.log(ans.first_number + ans.Second_number);
+    
+}else{
+    console.log(ans.first_number / ans.Second_number);
+    
+}
